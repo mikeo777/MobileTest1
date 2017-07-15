3dc8ac136f6797ae35a6872c04d726163c0a7202
Michael Offner <michaeloffner@hotmail.com>
2017-07-14 23:05:33 -0400

    Modified Global.asax.cs

--- MobileTest1/Global.asax.cs
+++ MobileTest1/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web.Routing;
 
 namespace MobileTest1
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : System.Web.HttpApplication 
     {
         protected void Application_Start()
         {
